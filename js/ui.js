9b834f6720fe571c89647f7bd841886129ed0ee6
Anton de Winter <adewinter@dimagi.com>
2011-07-24 08:35:51 -0400

    Added Object.keys method for backwards compatibility with older versions of FF

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,14 @@
 /*jslint maxerr: 50, indent: 4 */
 /*globals $,document,console*/
+
+if(!Object.keys) Object.keys = function(o){
+    if (o !== Object(o))
+        throw new TypeError('Object.keys called on non-object');
+    var ret=[],p;
+    for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+    return ret;
+}
+
 if (typeof formdesigner === 'undefined') {
     var formdesigner = {};
 }
