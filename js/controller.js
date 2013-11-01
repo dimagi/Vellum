dimagi/Vellum

commit ff7feef52a095bbb8034bfdbd4e885a53607d2e9

fix validation condition getting lost
The main change here is

-        if (this.__spec[attr] && this.__spec[attr].presence !== "notallowed") {
+
+        if (this.__spec[attr] && attr.indexOf('_') !== 0) {

Since all nodes initially get created as DataBindOnly mugs, because the
data tree (and then bind list) are parsed before the control block (and
then things get copied over to a new instance of the final control
node's question type), and proper Hidden Values have constraintAttr and
requiredAttr as "notallowed", this was preventing them from getting set.

It's ok to only reference notallowed on the side that displays validation
errors to the user.

This also changes some things to make setting and copying MugElement
attrs safer/more consistent.

js/controller.js
@@ -1475,17 +1475,13 @@ formdesigner.controller = (function () {
         }
         
         // create new mug and copy old data to newly generated mug
-        var options = {};
-        if (oldMug) {
-            options = {
-                dataElement: oldMug.dataElement,
-                bindElement: oldMug.bindElement
-            };
-        }
-        mug = new MugClass(options);
+        mug = new MugClass();
 
         if(oldMug) {
             mug.ufid = oldMug.ufid;
+
+            mug.bindElement.setAttrs(oldMug.bindElement || {});
+            mug.dataElement.setAttrs(oldMug.dataElement || {});
 
             //replace in dataTree
             that.form.replaceMug(oldMug, mug, 'data');

js/model.js
@@ -1406,7 +1406,8 @@ var MugElement = Class.$extend({
     },
     setAttr: function (attr, val) {
         // todo: replace all direct setting of element properties with this
-        if (this.__spec[attr] && this.__spec[attr].presence !== "notallowed") { 
+
+        if (this.__spec[attr] && attr.indexOf('_') !== 0) { 
             // avoid potential duplicate references (e.g., itext items)
             if (val && typeof val === "object") {
                 val = $.extend(true, {}, val);
@@ -1646,23 +1647,17 @@ var mugs = (function () {
         // whether you can change to or from this question's type in the UI
         isTypeChangeable: true,
         isODKOnly: false,
-        __init__: function (options) {
-            var options = options || {};
+        __init__: function () {
             var self = this;
             this.__spec = this.getSpec();
 
             _(this.__spec).each(function (spec, name) {
                 if (spec) {
-                    var newElement = new MugElement({
+                    self[name] = new MugElement({
                         spec: spec,
                         mug: self,
                         name: name
                     });
-                    var oldElement = options[name];
-                    if (oldElement) {
-                        newElement.setAttrs(oldElement);
-                    }
-                    self[name] = newElement;
                 } else {
                     self[name] = null;
                 }

commit 7c1308d38cd128f2f374cdd280eefcefdcbbb284

fix copying of attributes broken in ff7feef52

js/controller.js
@@ -471,12 +471,9 @@ formdesigner.controller = (function () {
          */
         function duplicateMug(mug, parentMug, options) {
             // clone mug and give everything new unique IDs
-            var duplicate = new mugs[mug.__className]({
-                bindElement: mug.bindElement,
-                controlElement: mug.controlElement,
-                dataElement: mug.dataElement
-            }),
+            var duplicate = new mugs[mug.__className]();
                 pathReplacements = [];
+            duplicate.copyAttrs(mug);
 
             // ensure consistency            
             formdesigner.util.give_ufid(duplicate);
@@ -675,16 +672,9 @@ formdesigner.controller = (function () {
                 }
             }
             
-            // get new mug and copy relevant data
-            var options = {};
-            var elems = ["dataElement", "bindElement", "controlElement"];
-            _(elems).each(function (elem) {
-                if (mug[elem]) {
-                    options[elem] = mug[elem];
-                }
-            });
-            var newMug = new mugs[questionType](options);
+            var newMug = new mugs[questionType]();
             newMug.ufid = mug.ufid;
+            newMug.copyAttrs(mug);
             
             // magic special cases.  todo: verify
             if (newMug.__className === "Select" || newMug.__className === "MSelect" || 
@@ -1476,12 +1466,9 @@ formdesigner.controller = (function () {
         
         // create new mug and copy old data to newly generated mug
         mug = new MugClass();
-
         if(oldMug) {
+            mug.copyAttrs(oldMug);
             mug.ufid = oldMug.ufid;
-
-            mug.bindElement.setAttrs(oldMug.bindElement || {});
-            mug.dataElement.setAttrs(oldMug.dataElement || {});
 
             //replace in dataTree
             that.form.replaceMug(oldMug, mug, 'data');

js/model.js
@@ -1703,6 +1703,17 @@ var mugs = (function () {
                 this
             );
         },
+        copyAttrs: function (sourceMug) {
+            if (this.dataElement && sourceMug.dataElement) {
+                this.dataElement.setAttrs(sourceMug.dataElement);
+            }
+            if (this.bindElement && sourceMug.bindElement) {
+                this.bindElement.setAttrs(sourceMug.bindElement);
+            }
+            if (this.controlElement && sourceMug.controlElement) {
+                this.controlElement.setAttrs(sourceMug.controlElement);
+            }
+        },
         getBindElementID: function () {
             if (this.bindElement) {
                 return this.bindElement.nodeID;