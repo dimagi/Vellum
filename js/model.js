dimagi/Vellum

commit 328ad807d88814c5a219bcf771fe81cae42170e5

fix constraint id not allowed error for groups
somewhere in the refactoring, where we started to use the property
definitions more than they were actually being used before, it caused an
issue where you would set the constraint itext id on all mugs as you
parsed the bindlist, and then when you parsed the control tree and saw
that a mug was a group/repeat/field list (which don't allow constraint
messages), it would still set the constraint itext id on that mug, which
caused an error to show up.

This fixes that by ignoring properties where the spec says they aren't
allowed when calling mugElement.setAttr().  This also paves the way for
eventually less worrying about what question types can be changed to
what other question types, because it would all be handled here.

js/model.js
@@ -1407,7 +1407,9 @@ var MugElement = Class.$extend({
     setAttr: function (attr, val) {
         // todo: replace all direct setting of element properties with this
 
-        if (this.__spec[attr] && attr.indexOf('_') !== 0) { 
+        var spec = this.__spec[attr];
+
+        if (spec && spec.presence !== 'notallowed' && attr.indexOf('_') !== 0) { 
             // avoid potential duplicate references (e.g., itext items)
             if (val && typeof val === "object") {
                 val = $.extend(true, {}, val);

commit 2b3798067b2d1ce73a5e465accb5e9d687dfcb56

fix issue introduced in 328ad807d8

js/model.js
@@ -1402,7 +1402,12 @@ var MugElement = Class.$extend({
 
         var spec = this.__spec[attr];
 
-        if (spec && spec.presence !== 'notallowed' && attr.indexOf('_') !== 0) { 
+        // only set attr if spec allows this attr, except if mug is a
+        // DataBindOnly (which all mugs are before the control block has been
+        // parsed) 
+        if (attr.indexOf('_') !== 0 && spec && (spec.presence !== 'notallowed' || 
+                this.__mug.__className === 'DataBindOnly'))
+        {
             // avoid potential duplicate references (e.g., itext items)
             if (val && typeof val === "object") {
                 val = $.extend(true, {}, val);