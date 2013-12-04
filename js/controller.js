5c727d1de2387aca4da6a88a826bf722474ac22a
Mike White <m@mwhite.info>
2013-12-04 17:53:44 -0500

    fix change missed in 1bac5e5

--- js/controller.js
+++ js/controller.js
@@ -1274,7 +1274,7 @@ formdesigner.controller = (function () {
         cProps.label = labelVal;
         
         var newLabelItext = function (mug) {
-            var item = ItextItem({
+            var item = new ItextItem({
                 id: mug.getDefaultLabelItextId()
             });
             Itext.addItem(item);
