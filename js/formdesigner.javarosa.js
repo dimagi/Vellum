5e9e96c8f97473a52c847eaf982aa112c145257f
Daniel Roberts <droberts@dimagi.com>
2014-03-03 10:48:27 -0500

    specify language in parse warning message

--- js/formdesigner.javarosa.js
+++ js/formdesigner.javarosa.js
@@ -1390,8 +1390,9 @@ formdesigner.plugins.javaRosa = function (options) {
                 formdesigner.controller.addParseWarningMsg(
                     "You have languages in your form that are not specified " +
                     "in the \"Languages\" page of the application builder. " +
-                    "The following languages will be deleted on save " +
-                    "unless you add them to the \"Languages\" page."
+                    "The following language will be deleted on save " +
+                    "unless you add it to the \"Languages\" page: " +
+                    lang + "."
                 );
                 return;
             }
