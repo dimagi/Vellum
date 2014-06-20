dimagi/Vellum

commit 4c391f92b31426c0c20ebbec56580b4bce303db3

major refactoring
Use RequireJS, eliminate the formdesigner singleton with controller,
model, and ui having tangled inter-references including a bunch of event
soup, in favor of well-defined coponents using events only for loose
coupling.

See https://gist.github.com/mwhite/10e42de5b24b3437a610 for additional
explanation.

This removes the recently added duplicate ID renaming on save, in favor
of, when the nodeID property is changed via a widget change,
short-circuiting the change if a sibling has the same nodeID, and then
behaving similarly to when the XPath editor has unsaved changes until
you've finished entering a valid nodeID.  In service of this, the
"unsaved changes" short-circuiting now fires on several other events
besides selecting a node:

- adding a question
- saving the form
- selecting any tools menu item
- moving a question

Currently these are all done explicitly, although it might be possible
to have fewer locations where the check is called by putting it in e.g.
beforeSerialize().

This also manages all of the media uploader dependencies using
RequireJS.

Fixes:

 - #59: use RequireJS
 - #84: choice icon in tree should change when moving from single to
   multiple answer or vice versa

src/widgets.js
@@ -1,0 +1,340 @@
+define([
+    'tpl!./templates/widget_control_keyvalue',
+    'underscore',
+    'jquery'
+], function (
+    widget_control_keyvalue,
+    _,
+    $
+) {
+    function getGroupName(path) {
+        return path.split("/")[0];
+    }
+
+    function getPropertyName(path) {
+        return path.split("/")[1];
+    }
+
+    var base = function(mug, options) {
+        // set properties shared by all widgets
+        var widget = {};
+        // this shared method provides fake inheritance, assuming
+        // it is called in a constructor on the object being constructed
+        // ^^ what?
+        widget.options = options;
+        widget.mug = mug;
+        widget.id = options.id;
+
+        widget.isDisabled = function () {
+            // requires widget.path to be set.  This only happens in
+            // normal.  Need to change widgets that inherit directly from
+            // base to use the path/property system.
+            if (!widget.path) {
+                return false;
+            }
+
+            return mug.form.vellum.isPropertyLocked(mug.getAbsolutePath(), 
+                                                    widget.path);
+        };
+
+        widget.getDisplayName = function () {
+            // use the display text, or the property name if none found
+            return this.definition.lstring ? this.definition.lstring : this.propName;
+        };
+
+        widget.getControl = function () {
+            throw ("must be overridden");
+        };
+
+        widget.setValue = function (val) {
+            // noop
+        };
+
+        widget.getValue = function () {
+            // noop
+        };
+
+        widget.updateValue = function () {
+            // When a widget's value changes, do whatever work you need to in 
+            // the model/UI to make sure we are in a consistent state.
+            
+            var isID = (['nodeID', 'defaultValue'].indexOf(widget.propName) !== -1),
+                val = widget.getValue();
+
+            if (isID && val.indexOf(' ') !== -1) { 
+                // attempt to sanitize nodeID and choice values
+                // TODO, still may allow some bad values
+                widget.setValue(val.replace(/\s/g, '_'));
+            }
+            
+            //short circuit the mug property changing process for when the
+            //nodeID is changed to empty-string (i.e. when the user backspaces
+            //the whole value).  This allows us to keep a reference to everything
+            //and rename smoothly to the new value the user will ultimately enter.
+            if (isID && val === "") {
+                return;
+            }
+            
+            widget.save();
+        };
+
+        widget.save = function () {
+            throw 'Not Implemented';
+        };
+
+        widget.getUIElement = function () {
+            return getUIElement(widget.getControl(), widget.getDisplayName(),
+                                !!widget.isDisabled());
+        };
+
+        return widget;
+    };
+
+    var normal = function(mug, options) {
+
+        var path = options.path,
+            inputID = path.split("/").join("-"),
+            disabled = options.disabled || false,
+            widget = base(mug, options);
+
+        widget.path = path;
+        widget.definition = mug.getPropertyDefinition(path);
+        widget.currentValue = mug.getPropertyValue(path);
+        widget.groupName = getGroupName(widget.path);
+        widget.propName = getPropertyName(widget.path);
+        widget.id = inputID;
+
+        widget.input = $("<input />")
+            .attr("id", inputID)
+            .prop('disabled', disabled);
+
+        widget.getControl = function () {
+            return widget.input; 
+        };
+
+        widget.save = function () {
+            this.mug.setPropertyValue(
+                this.groupName, this.propName, this.getValue(), this.mug);
+        };
+        return widget;
+    };
+
+    var text = function (mug, options) {
+        var widget = normal(mug, options),
+            input = widget.input;
+        input.attr("type", "text").addClass('input-block-level');
+
+        widget.setValue = function (value) {
+            if (value !== undefined) {
+                value = value.replace(
+                    new RegExp(String.fromCharCode(10), 'g'), '&#10;');
+            }
+            input.val(value);
+        };
+
+        widget.getValue = function() {
+            return input.val();
+        };
+
+        input.bind("change keyup", widget.updateValue);
+        return widget;
+    };
+
+    var droppableText = function (mug, options) {
+        var widget = text(mug, options);
+        widget.input.addClass('jstree-drop')
+            .attr('placeholder', 'Hint: drag a question here.')
+            .change(widget.updateValue);
+
+        return widget;
+    };
+
+    var checkbox = function (mug, options) {
+        var widget = normal(mug, options),
+            input = widget.input;
+        input.attr("type", "checkbox");
+
+        widget.setValue = function (value) {
+            input.prop("checked", value);
+        };
+
+        widget.getValue = function() {
+            return input.prop("checked");
+        };
+
+        input.change(widget.updateValue);
+        return widget;
+    };
+
+    var xPath = function (mug, options) {
+        var widget = text(mug, options);
+
+        var super_getValue = widget.getValue;
+        widget.getValue = function() {
+            var val = super_getValue();
+            if ($.trim(val) === "") {
+                return "";
+            }
+            return val;
+        };
+
+        widget.getUIElement = function () {
+            var elem = getUIElement(
+                widget.getControl(), widget.getDisplayName(), !!widget.isDisabled());
+            return getUIElementWithEditButton(elem, function () {
+                widget.options.displayXPathEditor({
+                    value: mug.getPropertyValue(options.path),
+                    xpathType: widget.definition.xpathType,
+                    done: function (val) {
+                        if (val !== false) {
+                            mug.setPropertyValue(
+                                widget.groupName, widget.propName, val);
+                        }
+                    }
+                });
+                if (window._gaq) {
+                    _gaq.push(['_trackEvent', 'Form Builder', 'Logic', options.lstring]);
+                }
+            }, !!widget.isDisabled());
+        };
+
+        return widget;
+    };
+
+    var baseKeyValue = function (mug, options) {
+        var widget = base(mug, options),
+            id = options.id;
+        widget.definition = {};
+
+        // todo make a style for this when vellum gets a facelift
+        widget.kvInput = $('<div class="control-row" />').attr('id', id);
+
+        widget.getControl = function () {
+            if (widget.isDisabled()) {
+                // todo
+            }
+            return widget.kvInput;
+        };
+
+        widget.setValue = function (value) {
+            widget.kvInput.html(widget_control_keyvalue({
+                pairs: value
+            }));
+            widget.kvInput.find('input').bind('change keyup', widget.updateValue);
+            widget.kvInput.find('.fd-kv-add-pair').click(function (e) {
+                widget.refreshControl();
+                e.preventDefault();
+            });
+            widget.kvInput.find('.fd-kv-remove-pair').click(function (e) {
+                $(this).parent().parent().remove();
+                widget.refreshControl();
+                widget.save();
+                e.preventDefault();
+            });
+        };
+
+        widget.getValue = function () {
+            var currentValues = {};
+            _.each(widget.kvInput.find('.fd-kv-pair'), function (kvPair) {
+                var $pair = $(kvPair);
+                currentValues[$pair.find('.fd-kv-key').val()] = $pair.find('.fd-kv-val').val();
+            });
+            return currentValues;
+        };
+
+        widget.getValidValues = function () {
+            var values = _.clone(widget.getValue());
+            if (values[""]) {
+                delete values[""];
+            }
+            return values;
+        };
+
+        widget.updateValue = function () {
+            var currentValues = widget.getValue();
+            if (!("" in currentValues)) {
+                widget.kvInput.find('.btn').removeClass('hide');
+                widget.kvInput.find('.fd-kv-remove-pair').removeClass('hide');
+            }
+            widget.save();
+        };
+
+        widget.refreshControl = function () {
+            widget.setValue(widget.getValue());
+        };
+
+        return widget;
+    };
+
+    
+    var readOnlyControl = function (mug, options) {
+        options.id = "readonly-control";
+        var widget = base(mug, options);
+        widget.definition = {};
+        widget.currentValue = $('<div>').append(mug.controlElementRaw).clone().html();
+        widget.propName = "Raw XML: ";
+
+        widget.getControl = function () {
+            var control = $("<p />").text(this.currentValue);
+            return control;
+        };
+
+        return widget;
+    };
+   
+    var getUIElementWithEditButton = function($uiElem, editFn) {
+        var input = $uiElem.find('input'),
+            isDisabled = input ? input.prop('disabled') : false;
+
+        var button = $('<button />')
+            .addClass("fd-edit-button pull-right")
+            .text("Edit")
+            .stopLink()
+            .addClass('btn')
+            .attr('type', 'button')
+            .prop('disabled', isDisabled)
+            .click(editFn);
+
+        $uiElem.find('label').after(button);
+        $uiElem.find('.controls').css('margin-right', '60px');
+        return $uiElem;
+    };
+    
+    var getUIElement = function($input, labelText, isDisabled) {
+        var uiElem = $("<div />").addClass("widget control-group"),
+            $controls = $('<div class="controls" />'),
+            $label = getLabel(labelText, $input.attr('id'));
+        $label.addClass('control-label');
+        uiElem.append($label);
+
+        $input.prop('disabled', !!isDisabled);
+        $controls.append($input);
+        uiElem.append($controls);
+        return uiElem;
+    };
+
+    function getLabel(text, forId) {
+        var $label = $("<label />")
+            .text(text);
+        if (forId) {
+            $label.attr("for", forId);
+        }
+        return $label;
+    }
+
+    return {
+        base: base,
+        normal: normal,
+        text: text,
+        droppableText: droppableText,
+        checkbox: checkbox,
+        xPath: xPath,
+        baseKeyValue: baseKeyValue,
+        readOnlyControl: readOnlyControl,
+        util: {
+            getUIElementWithEditButton: getUIElementWithEditButton,
+            getUIElement: getUIElement
+        }
+    };
+});
+

commit 8f789c22421a6cdf642ec67d6685ecda6b4ae72b

fix display of errors for current mug on property change
introduced in 4c391f9

src/core.js
@@ -1486,6 +1486,9 @@ define([
             $fieldsetContent = $sec.find('.fd-fieldset-content');
         options.properties.map(function (prop) {
             var elemWidget = prop.widget(mug, $.extend(prop.options, {
+                afterChange: function () {
+                    _this.showVisualValidation(mug);
+                },
                 displayXPathEditor: function (options) {
                     _this.data.core.currentlyEditedProperty = prop.options.path;
                     _this.displayXPathEditor(options);

src/widgets.js
@@ -64,6 +64,11 @@ define([
             return null;
         };
 
+        widget.handleChange = function () {
+            widget.updateValue();
+            options.afterChange();
+        };
+
         widget.updateValue = function () {
             // When a widget's value changes, do whatever work you need to in 
             // the model/UI to make sure we are in a consistent state.
@@ -150,7 +155,7 @@ define([
         };
 
         input.bind("change keyup", function () {
-            widget.updateValue();
+            widget.handleChange();
         });
         return widget;
     };
@@ -160,7 +165,7 @@ define([
         widget.input.addClass('jstree-drop')
             .attr('placeholder', 'Hint: drag a question here.')
             .change(function () {
-                widget.updateValue();
+                widget.handleChange();
             });
 
         return widget;
@@ -180,7 +185,7 @@ define([
         };
 
         input.change(function () {
-            widget.updateValue();
+            widget.handleChange();
         });
         return widget;
     };
@@ -244,7 +249,7 @@ define([
                 pairs: value
             }));
             widget.kvInput.find('input').bind('change keyup', function () {
-                widget.updateValue();
+                widget.handleChange();
             });
             widget.kvInput.find('.fd-kv-add-pair').click(function (e) {
                 widget.refreshControl();