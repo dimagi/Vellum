96a108ab979be5c162c88af10a934e5fcd066504
Daniel Miller <dmiller@dimagi.com>
2015-01-22 11:16:49 -0500

    Ignore unknown plugins (rather than crashing)

---
+++ tests/base.js
@@ -0,0 +1,27 @@
+/*jshint multistr: true */
+require([
+    'chai',
+    'jquery',
+    'underscore',
+    'tests/utils'
+], function (
+    chai,
+    $,
+    _,
+    util
+) {
+    var assert = chai.assert;
+
+    describe("The plugin manager", function () {
+        it("should ignore unknown plugins", function (done) {
+            util.init({
+                plugins: ["plugin-will-not-be-found"],
+                core: { onReady: function () {
+                    assert(!this.isPluginEnabled("plugin-will-not-be-found"),
+                        "loaded unknown plugin");
+                    done();
+                }
+            }});
+        });
+    });
+});
