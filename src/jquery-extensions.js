12729d25a5914e3a917786419b82ef3964b1106c
Jonathan Emord <jonathan.emord@gmail.com>
2015-06-02 14:34:13 -0400

    Fix jquery extension

--- src/jquery-extensions.js
+++ src/jquery-extensions.js
@@ -1,12 +1,9 @@
-(function ($) {
+define(["jquery"], function ($) {
     var originalVal = $.fn.val;
     $.fn.val = function(){
         if (this.hasClass('fake-textarea') || this.hasClass('fake-input')) {
             return $.fn.text.apply(this, arguments);
         }
-        var result = originalVal.apply(this,arguments);
-        if(arguments.length>0)
-            $(this).change();
-        return result;
+        return originalVal.apply(this,arguments);
     };
-})(jQuery);
+});
