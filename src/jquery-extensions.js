1a6745e03a378396fd2a6158c295355036469b1d
Daniel Miller <dmiller@dimagi.com>
2015-06-24 20:51:44 -0400

    Consolidate jQuery extensions

--- src/jquery-extensions.js
+++ src/jquery-extensions.js
@@ -1,4 +1,44 @@
-define(["jquery"], function ($) {
+define([
+    "jquery",
+    "underscore"
+], function ($, _) {
+
+    $.fn.popAttr = function (name) {
+        var removed = false,
+            val = this.attr(name);
+        try {
+            this.removeAttr(name);
+            removed = true;
+        } catch (e) {
+            // catch InvalidCharacterError due to \: in attribute name
+        }
+        if (removed && !_.isUndefined(val)) {
+            if (!this[0].poppedAttributes) {
+                this[0].poppedAttributes = {};
+            }
+            this[0].poppedAttributes[name] = val;
+        }
+        return val;
+    };
+
+    $.fn.stopLink = function() {
+        // stops anchor tags from clicking through
+        this.click(function (e) {
+            e.preventDefault();
+        });
+        return this;
+    };
+
+    $.fn.fdHelp = function () {
+        // creates a help popover, requires twitter bootstrap
+        this.append($('<i />').addClass('icon-question-sign'))
+            .popout({
+                trigger: 'focus',
+                html: true
+            });
+        return this;
+    };
+
     if (!$.fn.disableSelection) {
         // stolen from jquery-ui
         // https://github.com/jquery/jquery-ui/blob/c2224bf/ui/core.js#L299-L315
