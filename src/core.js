af98a78b0609a1821b4628dde86c74891b3e56bd
Jonathan Emord <jonathan.emord@gmail.com>
2015-06-28 17:11:21 -0400

    Handle drag and drop

--- src/core.js
+++ src/core.js
@@ -880,7 +880,7 @@ define([
             inst = $.jstree.reference(target);
         if (!inst && target.vellum("get") === source.vellum("get")) {
             // only when not dragging inside the tree
-            if (target.hasClass("jstree-drop")) {
+            if (target.hasClass("jstree-drop")  || target.parents('.jstree-drop').length > 0) {
                 data.helper.find('.jstree-icon').removeClass('jstree-er').addClass('jstree-ok');
             } else {
                 data.helper.find('.jstree-icon').removeClass('jstree-ok').addClass('jstree-er');
@@ -890,10 +890,14 @@ define([
         var vellum = $(data.data.obj.context).vellum("get"),
             target = $(data.event.target),
             inst = $.jstree.reference(target);
-        if (!inst && target.hasClass("jstree-drop") && vellum === target.vellum("get")) {
+
+        if (!inst && (target.hasClass("jstree-drop") || target.parents('.jstree-drop').length > 0) && vellum === target.vellum("get")) {
             if (data.data.origin) {
                 var node = data.data.origin.get_node(data.data.nodes[0]);
                 if (node.data && node.data.handleDrop) {
+                    if (!target.hasClass('jstree-drop')) {
+                        target = target.parents('.jstree-drop');
+                    }
                     node.data.handleDrop(target);
                 }
             }
