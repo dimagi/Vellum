b0fd15d3e549415b532f3a92a3d973e02bd0d3c2
Jonathan Emord <jonathan.emord@gmail.com>
2015-07-01 17:58:06 -0400

    Get rid of atwho inserted div

--- src/util.js
+++ src/util.js
@@ -398,6 +398,8 @@ define([
                     return value;
                 }
             }
+        }).on('inserted.atwho', function(atwhoEvent, $li, browserEvent) {
+            $(this).find('atwho-inserted').children().unwrap();
         });
 
         mug.on("teardown-mug-properties", function () {
