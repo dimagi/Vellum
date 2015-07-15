49e1c60b2ab1a1ce04a90af813be05bf0cd15e36
Jonathan Emord <jonathan.emord@gmail.com>
2015-07-14 20:24:46 -0400

    add tests for printing
    cherry picked from the original printing branch
    
    Conflicts:
    	src/intentManager.js
    	tests/intentManager.js

--- tests/intentManager.js
+++ tests/intentManager.js
@@ -4,15 +4,20 @@ define([
     'underscore',
     'jquery',
     'text!static/intentManager/intent-with-unknown-attrs.xml',
-    'text!static/intentManager/intent-with-no-mug.xml'
+    'text!static/intentManager/intent-with-no-mug.xml',
+    'text!static/intentManager/printing-intent.xml'
 ], function (
     util,
     chai,
     _,
     $,
     INTENT_WITH_UNKNOWN_ATTRS_XML,
-    INTENT_WITH_NO_MUG_XML
+    INTENT_WITH_NO_MUG_XML,
+    PRINTING_INTENT_XML
 ) {
+    var assert = chai.assert,
+        call = util.call;
+
     describe("The intent manager plugin", function() {
         before(function (done) {
             util.init({
@@ -29,6 +34,20 @@ define([
         it("should preserve intent tags with no mug", function () {
             util.loadXML(INTENT_WITH_NO_MUG_XML);
             util.assertXmlEqual(util.call("createXML"), INTENT_WITH_NO_MUG_XML);
+        });
+
+        describe("printing mug", function() {
+            before(function() {
+                util.loadXML(PRINTING_INTENT_XML);
+            });
+
+            it("should parse as a printing question", function() {
+                assert.strictEqual(util.getMug('/data/print_data').__className, 'PrintIntent');
+            });
+
+            it("should write the same as parse", function() {
+                util.assertXmlEqual(call('createXML'), PRINTING_INTENT_XML);
+            });
         });
     });
 });
