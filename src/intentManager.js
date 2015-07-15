9dd1d3f1be168669bb974705d38851d5ffeb7153
Jonathan Emord <jonathan.emord@gmail.com>
2015-07-15 01:05:13 -0400

    remove global

--- src/intentManager.js
+++ src/intentManager.js
@@ -13,7 +13,9 @@ define([
     $
 ) {
     "use strict";
-    var DEFAULT_XMLNS = "http://opendatakit.org/xforms";
+    var DEFAULT_XMLNS = "http://opendatakit.org/xforms",
+        unmappedIntentTags = {};
+
     function makeODKXIntentTag (nodeID, path) {
         return {
             androidIntentAppId: path || "",
@@ -33,6 +35,7 @@ define([
         });
         return store;
     };
+
     var writeInnerTagXML = function(xmlWriter, innerTag, store) {
         if (store) {
             _.each(store, function (ref, key) {
@@ -45,6 +48,7 @@ define([
             });
         }
     };
+
     function writeXML(xmlWriter, properties) {
         xmlWriter.writeStartElement('odkx:intent');
         xmlWriter.writeAttributeString("xmlns:odkx", properties.intentXmlns);
@@ -58,91 +62,86 @@ define([
         xmlWriter.writeEndElement('odkx:intent');
     }
 
-    var intentManager = function () {
-        var that = {};
-        that.unmappedIntentTags = {};
+    function parseIntentTagsFromHead(tags) {
+        unmappedIntentTags = {};
+        _.each(tags, function (tagXML) {
+            var $tag, tagId, newTag, xmlns;
+            $tag = $(tagXML);
 
-        that.parseIntentTagsFromHead = function (tags) {
-            _.each(tags, function (tagXML) {
-                var $tag, tagId, newTag, xmlns;
-                $tag = $(tagXML);
+            tagId = $tag.attr('id');
+            newTag = makeODKXIntentTag(tagId, $tag.attr('class'));
 
-                tagId = $tag.attr('id');
-                newTag = makeODKXIntentTag(tagId, $tag.attr('class'));
-
-                xmlns = $tag.attr('xmlns:odkx');
-                newTag.xmlns = xmlns || newTag.intentXmlns;
-                newTag.androidIntentExtra = parseInnerTags($tag, 'extra');
-                newTag.androidIntentResponse = parseInnerTags($tag, 'response');
-                var unknowns = newTag.unknownAttrs;
-                _.each(tagXML.attributes, function (attr) {
-                    if (attr.nodeName !== 'id' &&
-                        attr.nodeName !== 'class' &&
+            xmlns = $tag.attr('xmlns:odkx');
+            newTag.xmlns = xmlns || newTag.intentXmlns;
+            newTag.androidIntentExtra = parseInnerTags($tag, 'extra');
+            newTag.androidIntentResponse = parseInnerTags($tag, 'response');
+            var unknowns = newTag.unknownAttrs;
+            _.each(tagXML.attributes, function (attr) {
+                if (attr.nodeName !== 'id' &&
+                    attr.nodeName !== 'class' &&
                         attr.nodeName !== 'xmlns:odkx')
                     {
                         unknowns[attr.nodeName] = attr.nodeValue;
                     }
-                });
-                that.unmappedIntentTags[tagId] = newTag;
             });
-        };
+            unmappedIntentTags[tagId] = newTag;
+        });
+    }
 
-        that.getParsedIntentTagWithID = function (nodeID) {
-            var intentTag = null;
-            _.each(that.unmappedIntentTags, function (tag) {
-                if (tag.nodeID === nodeID) {
-                    intentTag = tag;
-                }
-            });
-            return intentTag;
-        };
+    function getParsedIntentTagWithID (nodeID) {
+        var intentTag = null;
+        _.each(unmappedIntentTags, function (tag) {
+            if (tag.nodeID === nodeID) {
+                intentTag = tag;
+            }
+        });
+        return intentTag;
+    }
 
-        that.syncMugWithIntent = function (mug) {
-            // called when initializing a mug from a parsed form
-            if (mug.__className === "AndroidIntent") {
-                var nodeID = mug.p.nodeID,
-                    tag = that.getParsedIntentTagWithID(nodeID);
-                if (!tag) {
-                    var path = (mug.intentTag) ? mug.intentTag.androidIntentAppId : null;
-                    tag = makeODKXIntentTag(nodeID, path);
-                }
-                mug.intentTag = tag;
-                mug.p.androidIntentAppId = tag.androidIntentAppId;
-                mug.p.androidIntentExtra = tag.androidIntentExtra;
-                mug.p.androidIntentResponse = tag.androidIntentResponse;
-                mug.p.unknownAttrs = tag.unknownAttrs;
-                mug.p.intentXmlns = tag.intentXmlns;
-                delete that.unmappedIntentTags[tag.nodeID];
+    function syncMugWithIntent (mug) {
+        // called when initializing a mug from a parsed form
+        if (mug.__className === "AndroidIntent") {
+            var nodeID = mug.p.nodeID,
+                tag = getParsedIntentTagWithID(nodeID);
+            if (!tag) {
+                var path = (mug.intentTag) ? mug.intentTag.androidIntentAppId : null;
+                tag = makeODKXIntentTag(nodeID, path);
+            }
+            mug.intentTag = tag;
+            mug.p.androidIntentAppId = tag.androidIntentAppId;
+            mug.p.androidIntentExtra = tag.androidIntentExtra;
+            mug.p.androidIntentResponse = tag.androidIntentResponse;
+            mug.p.unknownAttrs = tag.unknownAttrs;
+            mug.p.intentXmlns = tag.intentXmlns;
+            delete unmappedIntentTags[tag.nodeID];
+        }
+    }
+
+    function writeIntentXML (xmlWriter, tree) {
+        // make sure any leftover intent tags are still kept
+        _.each(unmappedIntentTags, function (tag) {
+            writeXML(xmlWriter, tag);
+        });
+
+        var intents,
+        getIntentMugs = function(node) {
+            var mug = node.getValue();
+            if (!mug || node.isRootNode) {
+                return null;
+            }
+            if (mug.options.dataType === 'intent') {
+                return mug;
+            } else {
+                return null;
             }
         };
-
-        that.writeIntentXML = function (xmlWriter, tree) {
-            // make sure any leftover intent tags are still kept
-            _.each(that.unmappedIntentTags, function (tag) {
-               writeXML(xmlWriter, tag);
+        intents = tree.treeMap(getIntentMugs);
+        if (intents.length > 0) {
+            intents.map(function (intentMug) {
+                writeXML(xmlWriter, intentMug.p);
             });
-
-            var intents,
-                getIntentMugs = function(node) {
-                    var mug = node.getValue();
-                    if (!mug || node.isRootNode) {
-                        return null;
-                    }
-                    if (mug.options.dataType === 'intent') {
-                        return mug;
-                    } else {
-                        return null;
-                    }
-                };
-            intents = tree.treeMap(getIntentMugs);
-            if (intents.length > 0) {
-                intents.map(function (intentMug) {
-                    writeXML(xmlWriter, intentMug.p);
-                });
-            }
-        };
-        return that;
-    };
+        }
+    }
 
     function androidIntentAppId(mug, options) {
         var widget = widgets.text(mug, options),
@@ -207,26 +206,22 @@ define([
 
     $.vellum.plugin("intents", {}, {
         loadXML: function (xml) {
-            var manager = intentManager(null);
-            this.data.intents.manager = manager;
-            this.data.intents.manager.parseIntentTagsFromHead(
-                $(xml).find('h\\:head, head')
-                    .children("odkx\\:intent, intent"));
-
+            parseIntentTagsFromHead($(xml).find('h\\:head, head')
+                                    .children("odkx\\:intent, intent"));
             this.__callOld();
         },
         contributeToHeadXML: function (xmlWriter, form) {
             this.__callOld();
-            this.data.intents.manager.writeIntentXML(xmlWriter, form.tree);
+            writeIntentXML(xmlWriter, form.tree);
         },
         handleNewMug: function (mug) {
             var ret = this.__callOld();
-            this.data.intents.manager.syncMugWithIntent(mug);
+            syncMugWithIntent(mug);
             return ret;
         },
         handleMugParseFinish: function (mug) {
             this.__callOld();
-            this.data.intents.manager.syncMugWithIntent(mug);
+            syncMugWithIntent(mug);
         },
         getMugTypes: function () {
             var types = this.__callOld();
