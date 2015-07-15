00bfec4e82503d19739138a757b377ad5c184790
Jonathan Emord <jonathan.emord@gmail.com>
2015-07-14 23:51:55 -0400

    Add test for intent with no mug

--- tests/intentManager.js
+++ tests/intentManager.js
@@ -3,13 +3,15 @@ define([
     'chai',
     'underscore',
     'jquery',
-    'text!static/intentManager/intent-with-unknown-attrs.xml'
+    'text!static/intentManager/intent-with-unknown-attrs.xml',
+    'text!static/intentManager/intent-with-no-mug.xml'
 ], function (
     util,
     chai,
     _,
     $,
-    INTENT_WITH_UNKNOWN_ATTRS_XML
+    INTENT_WITH_UNKNOWN_ATTRS_XML,
+    INTENT_WITH_NO_MUG_XML
 ) {
     describe("The intent manager plugin", function() {
         before(function (done) {
@@ -22,6 +24,11 @@ define([
         it("should preserve unknown attributes of intent tag", function () {
             util.loadXML(INTENT_WITH_UNKNOWN_ATTRS_XML);
             util.assertXmlEqual(util.call("createXML"), INTENT_WITH_UNKNOWN_ATTRS_XML);
+        });
+
+        it("should preserve intent tags with no mug", function () {
+            util.loadXML(INTENT_WITH_NO_MUG_XML);
+            util.assertXmlEqual(util.call("createXML"), INTENT_WITH_NO_MUG_XML);
         });
     });
 });
