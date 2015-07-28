2db46d4fd5742000eff66d8ebdc761e56b0735fa
Jonathan Emord <jemord@dimagi.com>
2015-07-28 17:08:12 -0400

    add tests

--- tests/richtext.js
+++ tests/richtext.js
@@ -54,43 +54,60 @@ define([
     function externalIcon () { return icon('fcc-fd-external-case'); }
     function unknownIcon () { return icon('fcc-help'); }
 
-    function makeBubble(xpath, dispValue, icon, templateFn) {
-        templateFn = templateFn || _.identity;
-        return $('<span>').attr({
+    function bubbleSpan(xpath, internal, templateFn) {
+        var span = $('<span>').addClass('label label-datanode').attr({
             contenteditable: false,
             draggable: true,
             'data-value': templateFn(xpath),
-        }).append(icon).append(dispValue);
+        });
+        if (internal) {
+            span.addClass('label-datanode-internal');
+        } else {
+            span.addClass('label-datanode-external');
+        }
+        return span;
     }
 
-    function makeOutputValue(xpath, dispValue, icon) {
-        return makeBubble(xpath, dispValue, icon, function(path) {
+    function makeBubble(xpath, dispValue, icon, internal, templateFn) {
+        templateFn = templateFn || _.identity;
+        return bubbleSpan(xpath, internal, templateFn).append(icon).append(dispValue);
+    }
+
+    function makeOutputValue(xpath, dispValue, icon, internal) {
+        return makeBubble(xpath, dispValue, icon, internal, function(path) {
             return '<output value="' + path + '" />';
         });
     }
 
+    function wrapWithDiv(el) { return $('<div>').append(el); }
+
     describe("Rich text utilities", function() {
         // path, display value, icon
         var conversions = [
-            ['/data/text', 'text', icon('fcc-fd-text')],
-            ["instance('casedb')/cases/case[@case_id = /data/case_id]/blah", 'blah', externalIcon()],
-            ["instance('casedb')/cases/case[@case_id = /data/case_id]", 'case', externalIcon()],
-            ["/data/where-did-this-come-from", 'where-did-this-come-from', unknownIcon()],
+            ['/data/text', 'text', icon('fcc-fd-text'), true],
+            ["instance('casedb')/cases/case[@case_id = /data/case_id]/blah", 'blah', externalIcon(), false],
+            ["instance('casedb')/cases/case[@case_id = /data/case_id]", 'case', externalIcon(), false],
+            ["/data/where-did-this-come-from", 'where-did-this-come-from', unknownIcon(), false],
         ];
 
         _.each(conversions, function(val) {
             it("from text to html: " + val[0], function() {
                 assert.strictEqual(
-                    widgets.util.replaceOuputRef(formShim, val[0]).html(),
-                    makeBubble(val[0], val[1], val[2]).html()
+                    wrapWithDiv(widgets.util.replaceOuputRef(formShim, val[0], false, true)).html(),
+                    wrapWithDiv(makeBubble(val[0], val[1], val[2], val[3])).html()
                 );
             });
 
             it("from text to html with output value: " + val[0], function() {
                 assert.strictEqual(
-                    widgets.util.replaceOuputRef(formShim, val[0], false, true).html(),
-                    makeOutputValue(val[0], val[1], val[2]).html()
+                    wrapWithDiv(widgets.util.replaceOuputRef(formShim, val[0])).html(),
+                    wrapWithDiv(makeOutputValue(val[0], val[1], val[2], val[3])).html()
                 );
+            });
+
+            it("from html to text: " + val[0], function() {
+                var bubble = $('<div>').append(makeBubble(val[0], val[1], val[2], val[3])).html();
+                assert.strictEqual(widgets.util.fromRichText(bubble), val[0]);
             });
         });
     });
