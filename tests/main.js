0b935ed82aacff0644f50a551ab56280d3b9951a
Jonathan Emord <jemord@dimagi.com>
2015-09-14 11:07:53 -0400

    add tests

--- tests/main.js
+++ tests/main.js
@@ -114,6 +114,7 @@ require(['jquery', 'jquery.vellum'], function ($) {
         'tests/saveToCase',
         'tests/setvalue',
         'tests/richText',
+        'tests/comments',
     ], function (
         options
     ) {
