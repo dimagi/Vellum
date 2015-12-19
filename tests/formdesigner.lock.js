dc39810e5976d41322c86e474e41c54ad574952a
Jenny Schweers <jschweers@dimagi.com>
2015-12-19 17:55:37 -0500

    perhaps fix type changer test

--- tests/formdesigner.lock.js
+++ tests/formdesigner.lock.js
@@ -133,9 +133,7 @@ require([
                 var btn = $(".btn.current-question");
                 assert(btn.length === 1);
                 btn.click();
-                assert.equal(
-                    btn.next().find('li:contains("Cannot Change Question Type")').length,
-                    +!bool);
+                assert.equal(!!btn.next().find("li:not(.dropdown-header)").length, bool);
             }
             it("shows the type changer for type-changeable questions", function () {
                 testTypeChangeable(true);
