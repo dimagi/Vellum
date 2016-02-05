ffbb7d98ce793ebf41d7b62c4769b930cf26c86d
Jonathan Emord <jemord@dimagi.com>
2016-02-05 17:52:13 -0500

    integrate xpath with banana parser

--- src/bananas.js
+++ src/bananas.js
@@ -1,7 +1,117 @@
-define([], function() {
+define([
+    'vellum/xpath',
+], function(
+    xpath
+) {
     var OUTSIDE_BANANA = 0,
         INSIDE_BANANA = 1,
-        DELIMITER = "🍌";
+        DELIMITER = "🍌",
+        defaultParser = xpath.createParser(xpath.makeXPathModels({}));
+
+    function parse(input, transformFn, xpathParser) {
+        input = getSymbols(input);
+        xpathParser = xpathParser || defaultParser;
+
+        // I hope no one used multiple 🍌 in their forms before..
+        var parsedBanana = parseBananas(input, transformFn);
+
+        try {
+            // we should really only accept bananas, but if they supply a nice
+            // looking xpath. let's turn them into pretty bananas
+            var parsed = xpathParser.parse(parsedBanana.nontransformedText);
+            // success! now let's turn that input into bananas
+            parsed = xpathParser.parse(parsed.toHashtag());
+            return transformHashtags(parsed, xpathParser.models, function(input) {
+                return transformFn(input);
+            });
+        } catch (err) {
+            // a bad xpath. let's just return the transformed bananas
+            return parsedBanana.text;
+        }
+    }
+
+    function transformHashtags(parsedHashtags, models, transformFn) {
+        var queue = [parsedHashtags],
+            EXPR = models.XPathInitialContextEnum.EXPR,
+            node, i, children, j, predicates;
+        while (queue.length > 0) {
+            node = queue.shift();
+            if (node instanceof models.XPathPathExpr) {
+                if (node.initial_context === EXPR) {
+                    queue.push(node.filter.expr);
+                    predicates = node.filter.predicates;
+                    for (i = 0; i < predicates.length; i++) {
+                        queue.push(predicates[i]);
+                    }
+                }
+            } else if (node instanceof models.HashtagExpr) {
+                node.toHashtag = function() {
+                    return transformFn(node.toHashtag());
+                };
+            }
+            children = node.getChildren();
+            for (i = 0; i < children.length; i++) {
+                queue.push(children[i]);
+                if (children[i].predicates && children[i].predicates.length) {
+                    predicates = children[i].predicates;
+                    for (j = 0; j < predicates.length; j++) {
+                        queue.push(predicates[j]);
+                    }
+                }
+            }
+        }
+        return parsedHashtags.toHashtag();
+    }
+
+    function parseBananas(input, transformFn) {
+        transformFn = transformFn || function (input) { return input; };
+        var state = OUTSIDE_BANANA,
+            strLen = input.length,
+            text = "",
+            nontransformedText = "",
+            currentReference = "";
+
+        for (var i = 0; i < strLen; i++) {
+            var current = input[i],
+                next = input[i+1];
+
+            if (state === OUTSIDE_BANANA) {
+                if (current === DELIMITER && next === DELIMITER) {
+                    text += DELIMITER;
+                    nontransformedText += DELIMITER;
+                    i++;
+                } else if (current === DELIMITER) {
+                    state = INSIDE_BANANA;
+                } else {
+                    text += current;
+                    nontransformedText += current;
+                }
+            } else if (state === INSIDE_BANANA) {
+                if (current === DELIMITER && next === DELIMITER) {
+                    currentReference += DELIMITER;
+                    i++;
+                } else if (current === DELIMITER) {
+                    state = OUTSIDE_BANANA;
+                    text += transformFn(currentReference);
+                    nontransformedText += currentReference;
+                    currentReference = "";
+                } else if (next !== undefined){
+                    currentReference += current;
+                } else {
+                    // end of string, shouldn't happen, but will not
+                    // overestimate users or Vellum devs
+                    text += DELIMITER + currentReference;
+                    nontransformedText += DELIMITER + currentReference;
+                    currentReference = "";
+                }
+            }
+        }
+
+        return {
+            text: text,
+            nontransformedText: nontransformedText,
+        };
+    }
 
     function getSymbols(string) {
         var index = 0;
@@ -21,55 +131,6 @@ define([], function() {
         }
         output.push(string.charAt(index));
         return output;
-    }
-
-    function parse(input, transform) {
-        input = getSymbols(input);
-        transform = transform || function (input) { return input; };
-
-        var state = OUTSIDE_BANANA,
-            strLen = input.length,
-            text = "",
-            references = [],
-            currentReference = "";
-
-        for (var i = 0; i < strLen; i++) {
-            var current = input[i],
-                next = input[i+1];
-
-            if (state === OUTSIDE_BANANA) {
-                if (current === DELIMITER && next === DELIMITER) {
-                    text += DELIMITER;
-                    i++;
-                } else if (current === DELIMITER) {
-                    state = INSIDE_BANANA;
-                } else {
-                    text += current;
-                }
-            } else if (state === INSIDE_BANANA) {
-                if (current === DELIMITER && next === DELIMITER) {
-                    currentReference += DELIMITER;
-                    i++;
-                } else if (current === DELIMITER) {
-                    state = OUTSIDE_BANANA;
-                    references.push(currentReference);
-                    text += transform(currentReference);
-                    currentReference = "";
-                } else if (next !== undefined){
-                    currentReference += current;
-                } else {
-                    // end of string, shouldn't happen, but will not
-                    // overestimate users or Vellum devs
-                    text += DELIMITER + currentReference;
-                    currentReference = "";
-                }
-            }
-        }
-
-        return {
-            text: text,
-            references: references,
-        };
     }
 
     return parse;
