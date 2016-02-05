ccf759eef0f075fab2dfad09ac1f4469780a36a3
Jonathan Emord <jemord@dimagi.com>
2016-02-05 17:52:13 -0500

    integrate xpath with banana parser

--- tests/bananas.js
+++ tests/bananas.js
@@ -8,45 +8,39 @@ define([
     var assert = chai.assert;
 
     describe("The 🍌 parser", function () {
-        var testCases = [
-            ["🍌#case/type/prop🍌", "#case/type/prop", ["#case/type/prop"], "prop"],
-            ["(🍌#case/type/prop🍌)", "(#case/type/prop)", ["#case/type/prop"], "(prop)"],
-            ["(🍌#case/type/prop🍌", "(#case/type/prop", ["#case/type/prop"], "(prop"],
-            [
-                "🍌#case/type/prop🍌 = 🍌#case/type/prop2🍌",
-                "#case/type/prop = #case/type/prop2",
-                ["#case/type/prop", "#case/type/prop2"],
-                "prop = prop2"
-            ],
-            ["🍌🍌", "🍌", []],
-            ["🍊you glad I didn't use 🍌", "🍊you glad I didn't use 🍌", []],
-            ["🍌#case/type/prop🍌 = 🍌", "#case/type/prop = 🍌", ["#case/type/prop"], "prop = 🍌"],
-            ["🍌#case/type/prop🍌 = 🍌🍌", "#case/type/prop = 🍌", ["#case/type/prop"], "prop = 🍌"],
-            ["🍌#case/type/🍌🍌prop🍌 = 🍌🍌", "#case/type/🍌prop = 🍌", ["#case/type/🍌prop"], "🍌prop = 🍌"],
-            ["🍌🍌#case/type/🍌🍌prop🍌 = 🍌🍌", "🍌#case/type/🍌prop🍌 = 🍌", []],
-        ];
-
-        function transform(input) {
+        function transformToProperty(input) {
             var ret = input.split('/');
             return ret[ret.length-1];
         }
 
+        var testCases = [
+            ["🍌#case/type/prop🍌", "#case/type/prop", "prop"],
+            ["(🍌#case/type/prop🍌)", "(#case/type/prop)", "(prop)"], 
+            ["(🍌#case/type/prop🍌", "(#case/type/prop", "(prop"],
+            [
+                "🍌#case/type/prop🍌 = 🍌#case/type/prop2🍌",
+                "#case/type/prop = #case/type/prop2",
+                "prop = prop2",
+            ],
+            ["🍌🍌", "🍌", "🍌"],
+            ["🍊you glad I didn't use 🍌", "🍊you glad I didn't use 🍌", "🍊you glad I didn't use 🍌"],
+            ["🍌#case/type/prop🍌 = 🍌", "#case/type/prop = 🍌",  "prop = 🍌"],
+            ["🍌#case/type/prop🍌 = 🍌🍌", "#case/type/prop = 🍌", "prop = 🍌"],
+            ["🍌#case/type/🍌🍌prop🍌 = 🍌🍌", "#case/type/🍌prop = 🍌", "🍌prop = 🍌"],
+            ["🍌🍌#case/type/🍌🍌prop🍌 = 🍌🍌", "🍌#case/type/🍌prop🍌 = 🍌", "🍌#case/type/🍌prop🍌 = 🍌"],
+        ];
+
         testCases.forEach(function (testCase) {
             var input = testCase[0],
-                output = testCase[1],
-                references = testCase[2],
-                transformed = testCase[3] || output;
+                outputNoTransform = testCase[1],
+                outputToProp = testCase[2];
 
-            it("should parse " + input + " into " + output, function() {
-                assert.strictEqual(bananas(input).text, output);
+            it("should parse " + input + " into " + outputNoTransform, function() {
+                assert.strictEqual(bananas(input), outputNoTransform);
             });
 
-            it("should return " + references + " from " + input, function() {
-                assert.sameMembers(bananas(input).references, references);
-            });
-
-            it("should transform " + input + " to " + transformed, function() {
-                assert.strictEqual(bananas(input, transform).text, transformed);
+            it("should parse " + input + " into " + outputToProp, function() {
+                assert.strictEqual(bananas(input, transformToProperty), outputToProp);
             });
         });
     });
