eab2575961e76a61340c668d3a825ec77be9db3a
Jenny Schweers <orange.jenny@gmail.com>
2016-03-03 08:17:30 -0500

    Revert "Move fuse to form"

--- tests/atwho.js
+++ tests/atwho.js
@@ -3,87 +3,153 @@ define([
     'chai',
     'jquery',
     'underscore',
-    'text!static/atwho/test1.xml',
+    'vellum/richText',
+    'vellum/atwho',
+    'vellum/util',
+    'tpl!vellum/templates/atwho_display'
 ], function (
     util,
     chai,
     $,
     _,
-    TEST1_XML
+    richText,
+    atwho,
+    real_util,
+    ATWHO_DISPLAY
 ) {
-    var assert = chai.assert;
-
-    function getFuseData(string) {
-        var form = util.call('getData').core.form,
-            fuseRes = form.fuse.search(string);
-        return fuseRes.length ? fuseRes[0] : null;
-    }
+    var assert = chai.assert,
+        mugs = [
+            {
+                ufid: 0,
+                absolutePath: '/data/one',
+                options: {
+                    icon: 'fcc fcc-text',
+                },
+                p: {
+                    nodeID: 'one',
+                },
+                displayName: 'One',
+            },
+            {
+                ufid: 1,
+                absolutePath: '/data/long',
+                options: {
+                    icon: 'fcc fcc-text',
+                },
+                p: {
+                    nodeID: 'long',
+                },
+                displayName: 'This is going to be a really long display name that should be truncated',
+            },
+            {
+                ufid: 2,
+                absolutePath: undefined,
+                options: {
+                    icon: 'fcc fcc-choice',
+                },
+                p: {
+                    nodeID: 'choice',
+                },
+                displayName: 'Not going to show up',
+            },
+            {
+                ufid: 3,
+                absolutePath: '/data/modeliteration/item',
+                options: {
+                    icon: 'fcc fcc-choice',
+                },
+                p: {
+                    nodeID: 'item',
+                },
+                displayName: undefined,
+            },
+        ],
+        form = {
+            vellum: {
+                getMugDisplayName: function (mug) {
+                    return mug.displayName;
+                },
+                data: {
+                    atwho: {}
+                },
+            },
+            formUuid: 'test',
+            getMugList: function () { return mugs; },
+        };
+    _.each(mugs, function(mug) {
+        mug.form = form;
+        mug.on = function () {};
+        // for tests in editor
+        mug.icon = mug.options.icon;
+        mug.name = mug.absolutePath;
+        mug.displayLabel = real_util.truncate(form.vellum.getMugDisplayName(mug));
+        real_util.eventuality(mug);
+    });
 
     describe("atwho", function() {
-        beforeEach(function(done) {
-            util.init({
-                javaRosa: {langs: ['en']},
-                core: { form: TEST1_XML, onReady: done },
-                features: {rich_text: false},
-                plugins: ['atwho','modeliteration'],
-            });
+        var atwhoData;
+        before(function() {
+            atwhoData = atwho.cachedMugData(0)(form);
         });
 
-        function getDisplayedAtwhoViews() {
-            return $('.atwho-view').filter(function() {
-                return $(this).css('display') === 'block';
-            });
-        }
-
-        function displayAtwho(callback) {
-            var mug = util.clickQuestion('one')[0];
-            var input = $('[name=property-relevantAttr]');
-            input.val('/data/').keyup();
-            assert.strictEqual(getDisplayedAtwhoViews().length, 1);
-            try {
-                callback(mug);
-            } catch (err) {
-                throw err;
-            } finally {
-                mug.fire('teardown-mug-properties');
-            }
-            assert(!getDisplayedAtwhoViews().length);
-        }
-
-        function assertNumAtwhoChoices(num) {
-            assert.strictEqual(getDisplayedAtwhoViews().find('li').length, num);
-        }
-
         it("should truncate the display label", function() {
-            var mug = getFuseData('one');
+            var mug = _.findWhere(atwhoData, {id: 0});
             assert.strictEqual(mug.displayLabel, "One");
-            mug = getFuseData('long');
+            mug = _.findWhere(atwhoData, {id: 1});
             assert.strictEqual(mug.displayLabel, "This is going to be a rea&hellip;");
         });
 
         it("should not show mugs without absolutePath", function() {
-            displayAtwho(function(mug) {
-                assert(!getDisplayedAtwhoViews().find('li:contains("choice1")').length);
-            });
+            assert(!_.findWhere(atwhoData, {id: 2}));
         });
 
-        // only valid for small sets of questions
-        it("should have each mug", function () {
-            displayAtwho(function(mug) {
-                assertNumAtwhoChoices(3);
-            });
+        it("should not show mugs that don't display in the question tree", function() {
+            assert(!_.findWhere(atwhoData, {id: 3}));
         });
 
-        it("should destroy the atwho container on mug removal", function() {
-            displayAtwho(function(mug) {
+        describe("in an editor", function() {
+            var el = $("<div id='input'><input /></div>"),
+                mug = mugs[0],
+                input, atwhoview;
+
+            function getDisplayedAtwhoViews() {
+                return $('.atwho-view').filter(function() {
+                    return $(this).css('display') === 'block';
+                });
+            }
+            before(function () {
+                $('.atwho-container').remove();
+                $("body").append(el);
+                input = el.children().first();
+                atwho._questionAutocomplete(input, mug);
+                input.val('/data/');
+                input.keyup();
+                atwhoview = getDisplayedAtwhoViews();
+                assert.strictEqual(atwhoview.length, 1);
+            });
+            after(function() {
+                input.atwho('destroy');
+                input.remove();
+            });
+
+            it("should pop up", function () {
+                assert.strictEqual(atwhoview.length, 1);
+            });
+
+            it("should have each mug", function () {
+                assert.strictEqual(atwhoview.length, 1);
+                var listItems = atwhoview.find('li');
+
+                _.each(listItems, function(item, index) {
+                    assert.strictEqual(listItems[index].innerHTML,
+                                       $(ATWHO_DISPLAY(mugs[index]))[0].innerHTML);
+                });
+            });
+
+            it("should destroy the atwho container on mug removal", function() {
+                assert.strictEqual(atwhoview.length, 1);
                 mug.fire('teardown-mug-properties');
                 assert(!getDisplayedAtwhoViews().length);
-            });
-        });
-
-        it("should not show itself in the results", function () {
-            displayAtwho(function(mug) {
-                assertNumAtwhoChoices(3);
             });
         });
     });
