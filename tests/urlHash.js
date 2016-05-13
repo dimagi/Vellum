313fb8e80a5df26d5a2d16f101185d6e247f9f49
Jonathan Emord <jonathan.emord@gmail.com>
2016-05-13 11:50:56 -0400

    Move tests to #form

--- tests/urlHash.js
+++ tests/urlHash.js
@@ -17,14 +17,14 @@ require([
 
     describe("URL Hash", function() {
         it("should select a mug that is in the form", function() {
-            window.location.hash = '#/data/group/question2';
+            window.location.hash = '#form/group/question2';
             util.loadXML(GROUP_RENAME_XML);
             var mug = call("getCurrentlySelectedMug");
             assert.equal(mug.absolutePath, "/data/group/question2");
         });
 
         it("should select first mug if hash isn't in form", function() {
-            window.location.hash = '#/data/group/not_in_form';
+            window.location.hash = '#form/group/not_in_form';
             util.loadXML(GROUP_RENAME_XML);
             var mug = call("getCurrentlySelectedMug");
             assert.equal(mug.absolutePath, "/data/group");
@@ -33,16 +33,16 @@ require([
         it("should change the hash when you add a question", function() {
             util.loadXML("");
             util.addQuestion("Text", "text");
-            assert.equal(window.location.hash, '#/data/text');
+            assert.equal(window.location.hash, '#form/text');
         });
 
         it("should change the hash when you delete a question", function() {
             util.loadXML("");
             util.addQuestion("Text", "text");
             util.addQuestion("Text", "text2");
-            assert.equal(window.location.hash, '#/data/text2');
+            assert.equal(window.location.hash, '#form/text2');
             util.deleteQuestion("/data/text2");
-            assert.equal(window.location.hash, '#/data/text');
+            assert.equal(window.location.hash, '#form/text');
         });
     });
 });
