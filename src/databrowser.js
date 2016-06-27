47b8b9af3cf1c05c7dd0530d596330de7d4d85a0
Jonathan Emord <jonathan.emord@gmail.com>
2016-06-27 13:42:10 -0400

    Data browser list should be flat

--- src/databrowser.js
+++ src/databrowser.js
@@ -264,21 +264,23 @@ define([
             nodes = node(source, null, info)(source, path).children;
         }
 
-        // move the parent data sources up one level to be equal to their child
-        var siblings = [];
-        _.each(nodes, function (node) {
-            siblings = siblings.concat(_.filter(node.children, function(child) {
+        function flattenNode(node) {
+            var subCases = _.filter(node.children, function(child) {
                 return child.children.length;
-            }));
+            });
             node.children = _.filter(node.children, function(child) {
                 return child.children.length === 0;
             });
-        });
+            subCases = _.flatten(_.map(subCases, flattenNode));
+            return [node].concat(subCases);
+        }
+        // data sources should be in a flat list instead of hierarchy
+        nodes = _.flatten(_.map(nodes, flattenNode));
 
         // done here for performance reasons. would be nice to be done after
         // every new hashtag, but only for the mugs that reference that hashtag
         fixFormReferences(vellum.data.core.form);
-        return nodes.concat(siblings);
+        return nodes;
     }
 
     function toggleExternalDataTree(vellum) {
