d5d12ea6a6673624724bcde4e959285568bc6089
Jonathan Emord <jonathan.emord@gmail.com>
2016-08-18 13:25:26 -0400

    Don't write unknown case hashtags to xml

--- src/logic.js
+++ src/logic.js
@@ -467,9 +467,13 @@ define([
         // returns object of hashtags. used for writing to xml
         // format {hashtag: xpath} (null is used for cases as they will be loaded later)
         referencedHashtags: function () {
+            var _this = this;
             return _.chain(this.reverse[CASE_REF_ID] || {})
                 .values()
                 .flatten(true)
+                .filter(function(ref) {
+                    return _this.form.isValidHashtag(ref.path);
+                })
                 .map(function(ref) {
                     return [ref.path, null];
                 }).object().value();
