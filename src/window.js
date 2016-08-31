7e3ebd71c6e90d9e2909c4c5c62a54e045d832cb
Jennifer Schweers <orange.jenny@gmail.com>
2016-08-31 10:39:50 -0400

    Don't use ready 'event'

--- src/window.js
+++ src/window.js
@@ -37,7 +37,9 @@ define([
             // start with accessory pane collapsed
             $(window).resize(adjustToWindow);
             $(document).scroll(adjustToWindow);
-            $(document).ready(adjustToWindow);
+            $(function() {
+                _this.adjustToWindow();
+            });
             _this.adjustToWindow();
         },
         adjustToWindow: function() {
