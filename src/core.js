192bbee936712cc8d9466d858227f19ff35b8d0e
Jennifer Schweers <orange.jenny@gmail.com>
2016-12-23 13:28:15 -0500

    Revert "Remove 'related' options"
    This reverts commit ddfd9597c71c5965ed1ff9d666f407dd13d40bd8.

--- src/core.js
+++ src/core.js
@@ -222,6 +222,9 @@ define([
                 };
 
                 groupData.questions = _.map(groupData.questions, getQuestionData);
+                if (groupData.related && groupData.related.length) {
+                    groupData.related = _.map(groupData.related, getQuestionData);
+                }
             });
         });
 
@@ -240,7 +243,7 @@ define([
                         groups: _.map(column, function(groupData) {
                             return {
                                 name: groupData.group[1],
-                                questions: _.map(groupData.questions, function(questionType) {
+                                questions: _.map(groupData.questions.concat(groupData.related || []), function(questionType) {
                                     var mugType = _this.data.core.mugTypes[questionType];
                                     return {
                                         slug: questionType,
@@ -283,6 +286,9 @@ define([
                 },
                 {
                     group: ["Select", 'Multiple Choice'],
+                    related: [
+                        "Choice"
+                    ],
                     questions: this.getSelectQuestions()
                 },
                 {
