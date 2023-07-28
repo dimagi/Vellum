e3a929b6543174548fbec033c27d9b257cd07604
Simon Kelly <skelly@dimagi.com>
2023-07-28 12:23:30 +0200

    test assessment

--- tests/commcareConnect.js
+++ tests/commcareConnect.js
@@ -5,6 +5,7 @@ define([
     'underscore',
     'vellum/commcareConnect',
     'text!static/commcareConnect/learn_module.xml',
+    'text!static/commcareConnect/assessment.xml',
 ], function (
     util,
     chai,
@@ -12,6 +13,7 @@ define([
     _,
     commcareConnect,
     LEARN_MODULE_XML,
+    ASSESSMENT_XML,
 ) {
     var assert = chai.assert,
         call = util.call;
@@ -27,12 +29,19 @@ define([
         it("should load and save a learn module", function () {
             util.loadXML(LEARN_MODULE_XML);
             var module = util.getMug("module_1");
-            console.log(module.p);
             assert.equal(module.__className, "ConnectLearnModule");
             assert.equal(module.p.name, "'module 1'");
             assert.equal(module.p.description, "'Module 1 is fun\nLearning is fun'");
             assert.equal(module.p.time_estimate, "2");
             util.assertXmlEqual(call("createXML"), LEARN_MODULE_XML);
+        });
+
+        it("should load and save a assessment", function () {
+            util.loadXML(ASSESSMENT_XML);
+            var module = util.getMug("test_assessment");
+            assert.equal(module.__className, "ConnectAssessment");
+            assert.equal(module.p.user_score, "/data/score");
+            util.assertXmlEqual(call("createXML"), ASSESSMENT_XML);
         });
     });
 });
