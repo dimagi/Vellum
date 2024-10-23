04db3a7d380c4f56eb2b1d6468857c2301173929
Jenny Schweers <orange.jenny@gmail.com>
2024-10-23 14:26:38 -0400

    Added webpack config file for dev

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './tests/main.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '_build'),
+    },
+};
