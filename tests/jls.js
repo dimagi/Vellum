70eb00cf2048b8172a80827266ed9858f544eb80
Jenny Schweers <orange.jenny@gmail.com>
2025-04-08 13:30:43 -0400

    Added require of jquery.vellum

--- tests/jls.js
+++ tests/jls.js
@@ -7,29 +7,31 @@ mocha.setup({
     timeout: '10000',
 });
 
-require(["jquery", "tests/jls2"], function ($, jls2) {
-    console.log("required something");
-    jls2.logSomething();
+require(["jquery.vellum"], function () {
+    require(["jquery", "tests/jls2"], function ($, jls2) {
+        console.log("required something");
+        jls2.logSomething();
 
-    // TODO: copied from tests/main.js
-    function runTests() {
-        function showTestResults() {
-            $("#resultsTab").click();
-            return false;
+        // TODO: copied from tests/main.js
+        function runTests() {
+            function showTestResults() {
+                $("#resultsTab").click();
+                return false;
+            }
+            $(".sidebar #mocha-stats").remove();
+            mocha.run();
+            // move progress indicator into sidebar
+            $("#mocha-stats").css({
+                "margin-top": "3em",
+                position: "relative",
+                left: 0,
+                top: 0
+            }).appendTo(".sidebar");
+            $("#mocha-stats li").css({display: "block"});
+            $("#mocha-stats li.progress").css({height: "40px"});
+            $("#mocha-stats li.passes a").click(showTestResults);
+            $("#mocha-stats li.failures a").click(showTestResults);
         }
-        $(".sidebar #mocha-stats").remove();
-        mocha.run();
-        // move progress indicator into sidebar
-        $("#mocha-stats").css({
-            "margin-top": "3em",
-            position: "relative",
-            left: 0,
-            top: 0
-        }).appendTo(".sidebar");
-        $("#mocha-stats li").css({display: "block"});
-        $("#mocha-stats li.progress").css({height: "40px"});
-        $("#mocha-stats li.passes a").click(showTestResults);
-        $("#mocha-stats li.failures a").click(showTestResults);
-    }
-    $('#run-tests').click(runTests);
+        $('#run-tests').click(runTests);
+    });
 });
