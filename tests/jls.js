5b111c971002dfb03e97ab6ad627015665d6eeb5
Jenny Schweers <orange.jenny@gmail.com>
2025-04-08 13:02:38 -0400

    Added webpack config for some js that does nothing
    Set up a couple of skeleton files with a nested require, like in tests/main.

---
+++ tests/jls.js
@@ -0,0 +1,7 @@
+// TODO: delete this file
+console.log("do absolutely nothing");
+
+require(["tests/jls2"], function (jls2) {
+    console.log("required something");
+    jls2.logSomething();
+});
---
+++ webpack/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+BASE_PATH = path.resolve(__dirname, '..')
+
+module.exports = {
+    mode: 'development',
+    entry: './tests/jls.js',
+    output: {
+        filename: 'vellum.bundle.js',
+    },
+    resolve: {
+        alias: {
+            'jquery.vellum': path.resolve(BASE_PATH, 'src', 'main'),
+            'vellum': path.resolve(BASE_PATH, 'src'),
+            'tests': path.resolve(BASE_PATH, 'tests'),
+        },
+    },
+};
