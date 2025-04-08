7bf45e67649e1bf1d59a524301d3d9b4fb91ee59
Jenny Schweers <orange.jenny@gmail.com>
2025-04-08 13:30:43 -0400

    Added require of jquery.vellum

--- src/main.js
+++ src/main.js
@@ -1,137 +1,3 @@
-/* global requirejs */
-requirejs.config({
-    // For some reason when using the map config as suggested by some of the
-    // plugins' documentation, and only when including vellum in another
-    // app, it tries to get requirejs-promise instead of
-    // requirejs-promise.js, so using packages instead.  This might be a bug
-    // that should be reported.
-    packages: [
-        {
-            name: 'less',
-            location: '../node_modules/@bracken/require-less',
-            main: 'less.js'
-        },
-        {
-            name: 'css',
-            location: '../node_modules/require-css',
-            main: 'css.js'
-        },
-        {
-            name: 'text',
-            location: '../node_modules/requirejs-text',
-            main: 'text.js'
-        },
-        {
-            name: 'tpl',
-            location: '../node_modules/requirejs-undertemplate',
-            main: 'tpl.js'
-        },
-        {
-            name: 'json',
-            location: '../node_modules/requirejs-plugins/src',
-            main: 'json.js'
-        },
-        {
-            name: "codemirror",
-            location: "../node_modules/codemirror",
-            main: "lib/codemirror",
-        },
-    ],
-    paths: {
-        'vellum': '.',
-
-        'CryptoJS': '../lib/sha1',
-        'diff-match-patch': '../lib/diff_match_patch',
-        'jquery': '../node_modules/jquery/dist/jquery',
-        'jquery.jstree': '../node_modules/jstree/dist/jstree',
-        'jstree-actions': '../node_modules/jstree-actions/jstree-actions',
-        'jquery.bootstrap': '../node_modules/bootstrap/dist/js/bootstrap',
-        'underscore': '../node_modules/underscore/underscore',
-        'XMLWriter': '../node_modules/XMLWriter/XMLWriter',
-
-        'xpath': '../node_modules/xpath/dist/js-xpath',
-
-        'langCodes': '../node_modules/langcodes/langs.json',
-
-        'save-button': '../lib/SaveButton',
-
-        'jsdiff': '../node_modules/jsdiff/diff',
-        'markdown-it': '../node_modules/markdown-it/dist/markdown-it',
-        'caretjs': '../node_modules/Caret.js/dist/jquery.caret',
-        'atjs': '../node_modules/at.js/dist/js/jquery.atwho',
-        'ckeditor': '../lib/ckeditor/ckeditor',
-        'ckeditor-jquery': '../lib/ckeditor/adapters/jquery',
-        'fusejs': '../node_modules/fuse.js/src/fuse'
-    },
-    shim: {
-        'CryptoJS': {
-            exports: 'CryptoJS'
-        },
-        'diff-match-patch': {
-            exports: 'diff_match_patch'
-        },
-
-        'jquery.jstree': {
-            deps: ['jquery', 'css!../node_modules/jstree/dist/themes/default/style'],
-            exports: '$.fn.jstree'
-        },
-        'jstree-actions': {
-            deps: ['jquery.jstree'],
-        },
-        'jquery.bootstrap': {
-            deps: ['jquery'],
-            exports: '$.fn.popover'
-        },
-        'XMLWriter': {
-            exports: 'XMLWriter'
-        },
-
-        'save-button': {
-            deps: ['jquery'],
-            exports: 'SaveButton'
-        },
-
-        'xpath': {
-            exports: 'xpath'
-        },
-        'jsdiff': {
-            exports: 'JsDiff'
-        },
-        'markdown-it': {
-            exports: 'markdown-it'
-        },
-        'caretjs': {
-            deps: ['jquery'],
-            exports: 'caretjs'
-        },
-        'atjs': {
-            deps: ['jquery', 'caretjs', 'css!../node_modules/at.js/dist/css/jquery.atwho'],
-            exports: 'atjs'
-        },
-        'ckeditor': {
-            exports: 'CKEDITOR'
-        },
-        'ckeditor-jquery': {
-            deps: ['jquery', 'ckeditor'],
-            exports: '$.fn.ckeditor'
-        },
-        'fusejs': {
-            exports: 'fusejs'
-        }
-    },
-    less: {
-        logLevel: 1
-    }
-});
-
-// If jQuery was loaded before RequireJS, use the existing instance.
-// http://www.manuel-strehl.de/dev/load_jquery_before_requirejs.en.html
-if (window.jQuery) {
-    define('jquery', [], function() {
-        return window.jQuery;
-    });
-}
-
 if (!window.gettext) {
     window.gettext = function (arg) { return arg; };
     window.ngettext = function (singular, plural, count) {
@@ -140,23 +6,24 @@ if (!window.gettext) {
 }
 
 define([
+    // TODO: uncomment these
     // begin buildmain.py delimiter
-    'vellum/core',
-    'vellum/ignoreButRetain',
-    'vellum/intentManager',
-    'vellum/itemset',
-    'vellum/javaRosa/plugin',
-    'vellum/datasources',
-    'vellum/lock',
-    'vellum/databrowser',
-    'vellum/commtrack',
-    'vellum/modeliteration',
-    'vellum/saveToCase',
-    'vellum/uploader',
-    'vellum/window',
-    'vellum/copy-paste',
-    'vellum/commander',
-    'vellum/commcareConnect',
+    //'vellum/core',
+    //'vellum/ignoreButRetain',
+    //'vellum/intentManager',
+    //'vellum/itemset',
+    //'vellum/javaRosa/plugin',
+    //'vellum/datasources',
+    //'vellum/lock',
+    //'vellum/databrowser',
+    //'vellum/commtrack',
+    //'vellum/modeliteration',
+    //'vellum/saveToCase',
+    //'vellum/uploader',
+    //'vellum/window',
+    //'vellum/copy-paste',
+    //'vellum/commander',
+    //'vellum/commcareConnect',
     // end buildmain.py delimiter
 ], function () {
     // adds $.vellum as a side-effect
