e6a91d56d96d381ca840ebd0dac256ea0f9b5565
Jenny Schweers <orange.jenny@gmail.com>
2025-04-09 14:13:49 -0400

    Restore commented-out code and early returns now that all modules can be imported

--- src/core.js
+++ src/core.js
@@ -1,6 +1,5 @@
 // the UI/ViewModel
 
-// TODO: uncomment these
 define([
     'require',
     'save-button',
@@ -70,11 +69,9 @@ define([
     atwho,
     debug
 ) {
-return;     // TODO: let this file run
     // Load these modules in the background after all runtime dependencies have
     // been resolved, since they're not needed initially.
-    // TODO: uncomment
-    /*setTimeout(function () {
+    setTimeout(function () {
         require([
             'codemirror',
             'codemirror/mode/xml/xml',
@@ -82,7 +79,7 @@ return;     // TODO: let this file run
             'CryptoJS',
             'vellum/expressionEditor',
         ], function () {});
-    }, 0);*/
+    }, 0);
 
     var isMac = util.isMac,
         HOTKEY_UNICODE = {
@@ -733,8 +730,7 @@ return;     // TODO: let this file run
             $textarea.val(this.data.core.failedLoadXML);
         }
 
-        // TODO: uncomment
-        /*codeMirror = require('codemirror').fromTextArea($textarea.get(0), {
+        codeMirror = require('codemirror').fromTextArea($textarea.get(0), {
             mode: 'xml',
             lineNumbers: true,
             viewportMargin: Infinity,
@@ -748,7 +744,7 @@ return;     // TODO: let this file run
             codeMirror.setSize('100%', bodyHeight - pHeight);
             codeMirror.refresh();
             codeMirror.focus();
-        });*/
+        });
     };
 
     fn.showExportModal = function(done) {
@@ -1951,10 +1947,9 @@ return;     // TODO: let this file run
     fn.displayXPathEditor = function(options) {
         options.headerText = gettext("Expression Editor");
         options.loadEditor = function($div, options) {
-            // TODO: uncomment
-            /*require(['vellum/expressionEditor'], function (expressionEditor) {
+            require(['vellum/expressionEditor'], function (expressionEditor) {
                 expressionEditor.showXPathEditor($div, options);
-            });*/
+            });
         };
         this.displaySecondaryEditor(options);
     };
@@ -2303,8 +2298,7 @@ return;     // TODO: let this file run
     };
 
     fn.send = function (formText, saveType) {
-        // TODO: uncomment
-        /*var CryptoJS = require('CryptoJS'),
+        var CryptoJS = require('CryptoJS'),
             _this = this,
             opts = this.opts().core,
             checkForConflict = false,
@@ -2370,7 +2364,7 @@ return;     // TODO: let this file run
                 _this.data.core.lastSavedXForm = formText;
                 _this._setURLHash(_this._propertiesMug);
             }
-        });*/
+        });
     };
 
     /**
