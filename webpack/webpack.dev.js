14a693ea98b6e6d6e7352ff421dac1e366a9225c
Jenny Schweers <orange.jenny@gmail.com>
2025-04-23 16:23:37 -0400

    Fixes to make tests run on github actions and update docs

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -5,4 +5,12 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'eval-cheap-module-source-map',
+    externals: {
+        // Necessary for tests
+        jquery: 'window.jQuery',
+
+        // Vellum expects bootstrap to be externally provided.
+        // No value needed because Vellum never directly references bootstrap.
+        "bootstrap": "window.nothing",
+    },
 });
