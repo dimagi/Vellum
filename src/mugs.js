2f434edf0d0f72dbf68a5165b6fc0edeb22f6de3
Jing Cheng <jcheng@dimagi.com>
2026-02-27 15:07:14 -0500

    refactor mugs: simplify public module wiring

--- src/mugs.js
+++ src/mugs.js
@@ -1,20 +1,20 @@
 import baseSpecs from "./mugs/mugBaseSpecs";
 import defaultOptions from "./mugs/mugDefaultOptions";
-import MugCore from "./mugs/mugCore";
+import Mug from "./mugs/mugCore";
 import MugMessages from "./mugs/mugMessages";
 import {deserializeXPath, serializeXPath, updateInstances} from "./mugs/mugXPath";
 import baseMugTypes from "./mugs/types/registry";
 import MugTypesManager from "./mugs/typesManager";
 
-MugTypesManager.Mug = MugCore;
+MugTypesManager.Mug = Mug;
 
 export default {
     defaultOptions: defaultOptions,
     baseMugTypes: baseMugTypes,
     MugTypesManager: MugTypesManager,
     MugMessages: MugMessages,
-    WARNING: MugCore.WARNING,
-    ERROR: MugCore.ERROR,
+    WARNING: Mug.WARNING,
+    ERROR: Mug.ERROR,
     baseSpecs: baseSpecs,
     deserializeXPath: deserializeXPath,
     serializeXPath: serializeXPath,
