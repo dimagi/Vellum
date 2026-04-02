8ab93ae4664bab710708ff917a61854a49286a57
Evan Joseph-Pinero <ejosephpinero@gmail.com>
2026-04-02 13:56:48 -0700

    Add Locked option on questions with setter for vellum:lock property

--- src/lock.js
+++ src/lock.js
@@ -7,6 +7,7 @@
  */
 import $ from "jquery";
 import "vellum/core";
+import widgets from "vellum/widgets";
 
 const LOCKED_BIND_ATTR = "vellum:lock"
 
@@ -16,8 +17,38 @@ $.vellum.plugin("lock", {}, {
         const locked = el.xmlAttr(LOCKED_BIND_ATTR);
         if (locked && locked === 'all') {
             const mug = form.getMugByPath(path);
-            mug.p.set('locked', 'all');
+            mug.p.set('locked', true);
         }
+    },
+    getMainProperties: function () {
+        const properties = this.__callOld();
+        properties.splice(1 + properties.indexOf('requiredAttr'), 0, 'locked');
+        return properties;
+    },
+    getMugSpec: function () {
+        const spec = this.__callOld();
+        const isEditable = this.opts().features.edit_locked_questions;
+
+        spec.databind.locked = {
+            lstring: gettext("Locked"),
+            visibility: isEditable ? 'visible' : 'visible_if_present',
+            presence: 'optional',
+            widget: widgets.checkbox,
+            enabled: function (mug) { return isEditable },
+            help: gettext("A locked question cannot be edited, moved, or deleted from the form."),
+            helpURL: "https://www.example.com",  // placeholder for public documentation
+            serialize: () => {},
+            deserialize: () => {},
+            setter: function (mug, attr, value) {
+                if (value === true) {
+                    mug.p.rawBindAttributes[LOCKED_BIND_ATTR] = 'all';
+                } else {
+                    delete mug.p.rawBindAttributes[LOCKED_BIND_ATTR];
+                }
+                mug.p.set(attr, value);
+            },
+        };
+        return spec;
     },
     isPropertyLocked: function (mug, mugPath, propertyPath) {
         return this.__callOld() || mug.p.locked;
